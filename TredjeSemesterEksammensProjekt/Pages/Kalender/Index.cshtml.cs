bc86a3cdd74529b8046e18ed0ab1f730f1ba5c51
123qq1 <jeppe.bjerremand@gmail.com>
2022-11-09 13:08:25 +0100

    Oprettelse af sider til horizontal prototype

---
+++ TredjeSemesterEksammensProjekt/Pages/Kalender/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TredjeSemesterEksammensProjekt.Pages.Kalender
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ TredjeSemesterEksammensProjekt/Pages/Sælger/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TredjeSemesterEksammensProjekt.Pages.Sælger
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
