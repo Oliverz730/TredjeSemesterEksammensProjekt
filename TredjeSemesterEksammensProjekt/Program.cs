cabec831b15480f52d20cc7a22e362c3add82703
123qq1 <jeppe.bjerremand@gmail.com>
2022-11-10 13:18:49 +0100

    Added claims for Tekniker Sælger Konsulent and Konverter, with claims database

--- TredjeSemesterEksammensProjekt/Program.cs
+++ TredjeSemesterEksammensProjekt/Program.cs
@@ -1,18 +1,46 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TredjeSemesterEksammensProjekt.Data;
+using TredjeSemesterEksammensProjekt.UserDbContextProjekt;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+
+
+
+var connectionString = builder.Configuration.GetConnectionString("WebAppUserDbContextConnection");
+builder.Services.AddDbContext<WebAppUserDbContext>(options =>
+    options.UseSqlServer(connectionString,
+    x=> x.MigrationsAssembly("UserDbMigrationProjekt"))
+    );
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<WebAppUserDbContext>();
+
+
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddRazorPages();
+//Add Claims
+builder.Services.AddAuthorization(options => 
+{
+    options.AddPolicy("TeknikerPolicy", policyBuilder => policyBuilder.RequireAssertion(context => context.User.HasClaim(c => c.Type == "Tekniker" || c.Type == "Admin")));
+    options.AddPolicy("SælgerPolicy", policyBuilder => policyBuilder.RequireAssertion(context => context.User.HasClaim(c => c.Type == "Sælger" || c.Type == "Admin")));
+    options.AddPolicy("KonverterPolicy", policyBuilder => policyBuilder.RequireAssertion(context => context.User.HasClaim(c => c.Type == "Konverter" || c.Type == "Admin")));
+    options.AddPolicy("KonsulentPolicy", policyBuilder => policyBuilder.RequireAssertion(context => context.User.HasClaim(c => c.Type == "Konsulent" || c.Type == "Admin")));
+
+    options.AddPolicy("AdminPolicy", policyBuilder => policyBuilder.RequireClaim("Admin"));
+}
+);
+
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Tekniker", "TeknikerPolicy");
+    options.Conventions.AuthorizeFolder("/Sælger", "SælgerPolicy");
+    options.Conventions.AuthorizeFolder("/Konverter", "KonverterPolicy");
+    options.Conventions.AuthorizeFolder("/Konsulent", "KonsulentPolicy");
+});
 
 var app = builder.Build();
 
