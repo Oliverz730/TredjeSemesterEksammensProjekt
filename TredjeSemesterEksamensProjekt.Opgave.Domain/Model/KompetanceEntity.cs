2115ba5252467d9943b029ce78c053aa6e1dd812
123qq1 <jeppe.bjerremand@gmail.com>
2022-11-13 08:28:46 +0100

    Added Opgave and Kompetance entity to domain

---
+++ TredjeSemesterEksamensProjekt.Opgave.Domain/Model/KompetanceEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TredjeSemesterEksamensProjekt.Opgave.Domain.Model
+{
+    public class KompetanceEntity
+    {
+        public string Description { get; private set; }
+
+        public KompetanceEntity(string description)
+        {
+            Description = description;
+        }
+    }
+}
---
+++ TredjeSemesterEksamensProjekt.Opgave.Domain/Model/OpgaveEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TredjeSemesterEksamensProjekt.Opgave.Domain.Model
+{
+    public class OpgaveEntity
+    {
+        public int Id { get; private set; }
+        public TimeSpan Duration { get; private set; }
+        public string Status { get; private set; }
+        public KompetanceEntity Kompetance { get; private set; }
+     
+        public OpgaveEntity(TimeSpan duration, string status, KompetanceEntity kompetance)
+        {
+            Duration = duration;
+            Status = status;
+            Kompetance = kompetance;
+        }
+
+    }
+}
