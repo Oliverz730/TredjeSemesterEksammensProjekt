e685045ef180233d3a0b6e483c077ea164878513
Pernlle <pkoe37782@edu.ucl.dk>
2022-11-23 19:42:37 +0100

    Entity -book, proj

---
+++ TSEP.StamData.Domain/Model/BookingEntity.cs
@@ -0,0 +1,17 @@
+﻿
+namespace TSEP.StamData.Domain.Model
+{
+    public class BookingEntity
+    {
+        public int Id { get; private set; }
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+        public BookingEntity(int id, DateTime startDate, DateTime endDate)
+        {
+            Id = id;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+        internal BookingEntity() {}
+    }
+}
