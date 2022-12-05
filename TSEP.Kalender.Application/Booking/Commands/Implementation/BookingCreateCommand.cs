121b9802abea6ffa539283bf89036c47c2b6f661
Pernlle <pkoe37782@edu.ucl.dk>
2022-12-05 09:39:04 +0100

    This time Added

---
+++ TSEP.Kalender.Application/Booking/Commands/Implementation/BookingCreateCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSEP.Kalender.Application.Booking.Commands.Implementation
+{
+    public class BookingCreateCommand
+    {
+    }
+}
