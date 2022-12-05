c141095c84c86edd0019549a87924634203694be
Glenn Sittrup Olesen <glenn.s.olesen@gmail.com>
2022-12-05 11:50:29 +0100

    domainservices added

--- TSEP.Kalender.Application/Booking/Commands/Implementation/BookingCreateCommand.cs
+++ TSEP.Kalender.Application/Booking/Commands/Implementation/BookingCreateCommand.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TSEP.Kalender.Application.Booking.Repositories;
+using TSEP.Kalender.Domain.DomainServices;
 using TSEP.Kalender.Domain.Model;
 
 namespace TSEP.Kalender.Application.Booking.Commands.Implementation
@@ -11,16 +12,19 @@ namespace TSEP.Kalender.Application.Booking.Commands.Implementation
     public class BookingCreateCommand : IBookingCreateCommand
     {
         private readonly IBookingRepository _bookingRepository;
+        private readonly IBookingDomainService _domainService;
 
-        public BookingCreateCommand(IBookingRepository bookingRepository)
+        public BookingCreateCommand(IBookingRepository bookingRepository, IBookingDomainService domainService)
         {
             _bookingRepository = bookingRepository;
+            _domainService = domainService;
         }
 
         void IBookingCreateCommand.Create(BookingCreateRequestDto bookingCreateRequestDto)
         {
 
             var booking = new BookingEntity(
+                _domainService,
                 bookingCreateRequestDto.Id,
                 bookingCreateRequestDto.StartDate,
                 bookingCreateRequestDto.EndDate
---
+++ TSEP.Kalender.Infrastructor/DomainServices/BookingDomainService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSEP.Kalender.Domain.DomainServices;
+using TSEP.SqlDbContext;
+
+namespace TSEP.Kalender.Infrastructor.DomainServices
+{
+    public class BookingDomainService : IBookingDomainService
+    {
+        private readonly TredjeSemesterEksamensProjektContext _db;
+        public BookingDomainService(TredjeSemesterEksamensProjektContext db)
+        {
+            _db = db;
+        }
+        bool IBookingDomainService.BookingExsistsOnDate(DateTime startDate, DateTime endDate)
+        {
+            return _db.BookingEntities.AsNoTracking().ToList().Any(a => a.Date.Date == date.Date);
+        }
+    }
+}
