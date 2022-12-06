8aec1256349d1ae631169cd8b4e10973d7552950
Glenn Sittrup Olesen <glenn.s.olesen@gmail.com>
2022-12-06 13:11:09 +0100

    dumme mapper

---
+++ TSEP.Kalender.DomainTest/BookingEntityCreateTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSEP.Kalender.Domain.Model;
+using TSEP.Kalender.Domain.DomainServices;
+using Moq;
+
+namespace TSEP.Kalender.DomainTest
+{
+    public class BookingEntityCreateTest
+    {
+        [Theory]
+        [InlineData(3000000)]
+        [InlineData(1)]
+
+        public void Given_StartDate_Is_Before_EndDate_And_No_Overlap_BookingEntity_Is_Created(int offset)
+        {
+            //Arrange
+            DateTime start = DateTime.Now;
+            DateTime end = start + TimeSpan.FromSeconds(offset);
+
+            var mock = new Mock<IBookingDomainService>();
+            mock.Setup(m => m.BookingExsistsOnDate(It.IsAny<DateTime>(), It.IsAny<DateTime>())).Returns(false);
+
+            //Act
+            var sut = new BookingEntity(mock.Object, 1, start, end);
+
+            //Assert
+        }
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-3000)]
+        public void Given_StartDate_Is_After_EndDate_And_No_Overlap_Then_ArgumentException_Is_Thrown(int offset)
+        {
+
+            //Arrange
+            DateTime start = DateTime.Now;
+            DateTime end = start + TimeSpan.FromSeconds(offset);
+
+            var mock = new Mock<IBookingDomainService>();
+            mock.Setup(m => m.BookingExsistsOnDate(It.IsAny<DateTime>(), It.IsAny<DateTime>())).Returns(false);
+
+            //Act
+
+            //Assert
+            Assert.Throws<ArgumentException>(() => new BookingEntity(mock.Object, 1, start, end));
+        }
+
+        [Fact]
+        public void Given_Date_Has_No_Overlap_Then_BookingEntity_Is_Created()
+        {
+
+            //Arrange
+            DateTime start = DateTime.Now;
+            DateTime end = start + TimeSpan.FromSeconds(1000);
+
+            var mock = new Mock<IBookingDomainService>();
+            mock.Setup(m => m.BookingExsistsOnDate(It.IsAny<DateTime>(), It.IsAny<DateTime>())).Returns(false);
+
+            //Act
+            var sut = new BookingEntity(mock.Object, 1, start, end);
+
+            //Assert
+        }
+        [Fact]
+        public void Given_Date_Has_Overlap_Then_ArgumentException_Is_Thrown()
+        {
+
+            //Arrange
+            DateTime start = DateTime.Now;
+            DateTime end = start + TimeSpan.FromSeconds(1000);
+
+            var mock = new Mock<IBookingDomainService>();
+            mock.Setup(m => m.BookingExsistsOnDate(It.IsAny<DateTime>(), It.IsAny<DateTime>())).Returns(true);
+
+            //Act
+
+            //Assert
+            Assert.Throws<ArgumentException>(() => new BookingEntity(mock.Object, 1, start, end));
+        }
+    }
+}
