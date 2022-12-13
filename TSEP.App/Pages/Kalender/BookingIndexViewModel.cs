8051f4519f738804dda4cb177bf185d0d51180f7
123qq1 <jeppe.bjerremand@gmail.com>
2022-12-13 11:12:47 +0100

    Made Kalender page a bit prettier

--- TSEP.App/Pages/Kalender/BookingIndexViewModel.cs
+++ TSEP.App/Pages/Kalender/BookingIndexViewModel.cs
@@ -1,10 +1,11 @@
 ﻿namespace TSEP.App.Pages.Kalender
 {
-    public class KalenderIndexViewModel
+    public class BookingIndexViewModel
     {
         public int Id { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public byte[] Rowversion { get; set; }
+        public string AnsatNavn { get; set; }
     }
 }
--- TSEP.App/Pages/Kalender/Index.cshtml.cs
+++ TSEP.App/Pages/Kalender/Index.cshtml.cs
@@ -1,11 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TSEP.App.Infrastructure.Kalender.Contract;
+using TSEP.App.Infrastructure.StamData.Contract;
 
 namespace TSEP.App.Pages.Kalender
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly IKalenderService _kalenderService;
+        private readonly IStamDataService _stamDataService;
+
+        public IndexModel(IKalenderService kalenderService, IStamDataService stamDataService)
         {
+            _kalenderService = kalenderService;
+            _stamDataService = stamDataService;
+        }
+
+        [BindProperty]
+        public List<BookingIndexViewModel> BookingIndexList { get; set; }
+
+        public async Task<ActionResult> OnGetAsync()
+        {
+            var ansatte = await _stamDataService.GetAllAnsat();
+            var bookings = await _kalenderService.GetAllBookings();
+
+            BookingIndexList = bookings.Select(b => new BookingIndexViewModel
+            {
+                Id = b.Id,
+                AnsatNavn = ansatte.First(a => a.UserId == b.MedarbejderId).Name,
+                EndDate = b.EndDate,
+                Rowversion = b.RowVersion,
+                StartDate = b.StartDate
+            }).ToList();
+
+            BookingIndexList = BookingIndexList.OrderBy(b => b.StartDate).ToList();
+
+            return Page();
         }
     }
 }
