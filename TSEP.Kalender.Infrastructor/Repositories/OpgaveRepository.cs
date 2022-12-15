e7cc95de168713f8df51f8fc2bc342b99699725c
Pernlle <pkoe37782@edu.ucl.dk>
2022-12-15 10:36:35 +0100

    Push'er branch

--- TSEP.Kalender.Infrastructor/Repositories/OpgaveRepository.cs
+++ TSEP.Kalender.Infrastructor/Repositories/OpgaveRepository.cs
@@ -22,16 +22,21 @@ namespace TSEP.Kalender.Infrastructor.Repositories
 
         void IOpgaveRepository.Add(OpgaveEntity opgave)
         {
+            // Tilføj Opgave til databasen
             _db.OpgaveEntities.Add(opgave);
+            // Gem ændringer i databasen
             _db.SaveChanges();
         }
 
         OpgaveQueryResultDto IOpgaveRepository.Get(int projektId, int opgaveTypeId, string ansatId)
         {
+            //Find opgave med det givne projektId, opgaveId og ansatId
             var opgave = _db.OpgaveEntities.First(o =>
                 o.ProjektId == projektId && o.OpgaveTypeId == opgaveTypeId && o.AnsatId == ansatId);
+            //Hvis dette ikke er muligt throw exception ""
             if (opgave == null) throw new Exception("Opgave findes ikke");
 
+            // Returner -konvertering fra Entity til OpgaveQueryResultDto
             return new OpgaveQueryResultDto
             {
                 ProjektId = opgave.ProjektId,
@@ -47,8 +52,10 @@ namespace TSEP.Kalender.Infrastructor.Repositories
 
         IEnumerable<OpgaveQueryResultDto> IOpgaveRepository.GetAllByAnsat(string ansatId)
         {
+            //Itterér over listen af opgaver, hvor givne asnatId matcher
             foreach (var opgave in _db.OpgaveEntities.Where(o => o.AnsatId == ansatId).AsNoTracking().ToList())
             {
+                //Konverter fra Entity til OpgaveQueryResultDto, og yield hver enkeltvis
                 yield return new OpgaveQueryResultDto
                 {
                     ProjektId = opgave.ProjektId,
@@ -65,8 +72,10 @@ namespace TSEP.Kalender.Infrastructor.Repositories
 
         IEnumerable<OpgaveQueryResultDto> IOpgaveRepository.GetAllByProjekt(int projektId)
         {
+            //Itterér over listen af opgaver, hvor givne projektId matcher
             foreach (var opgave in _db.OpgaveEntities.Where(o => o.ProjektId == projektId).AsNoTracking().ToList())
             {
+                //Konverter fra Entity til OpgaveQueryResultDto, og yield hver enkeltvis
                 yield return new OpgaveQueryResultDto
                 {
                     ProjektId = opgave.ProjektId,
